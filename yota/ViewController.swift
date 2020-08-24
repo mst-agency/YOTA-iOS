474afd45df20ad3defcc81e96bc0e17f78150d03
Victor <41268339+vevt@users.noreply.github.com>
2020-08-24 18:05:27 +0300

    Add initial project files

---
+++ yota/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  yota
+//
+//  Created by Dexter on 24.08.2020.
+//  Copyright © 2020 MST. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
