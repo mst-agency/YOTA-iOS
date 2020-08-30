6cf39cc01564cd2be7c7fb82d5b41b99c0f48c6f
Anton Belov <anb@mstagency.ru>
2020-08-31 02:07:19 +0300

    Added screen definition
    # Conflicts:
    #	yota.xcodeproj/project.pbxproj

---
+++ yota/Helpers/ScreenType.swift
@@ -0,0 +1,17 @@
+//
+//  ScreenType.swift
+//  yota
+//
+//  Created by Anton on 30.08.2020.
+//  Copyright © 2020 MST. All rights reserved.
+//
+
+import Foundation
+
+enum ScreenType: String {
+    case iphoneX
+    case iphone6plus
+    case iphone6
+    case iphone5
+    case ipad
+}
