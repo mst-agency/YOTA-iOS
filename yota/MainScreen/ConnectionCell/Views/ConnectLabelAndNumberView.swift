5444c584a0963f86278ecc2bc5f81a8ac23c7338
Anton Belov <anb@mstagency.ru>
2020-09-01 00:50:50 +0300

    Added views

---
+++ yota/MainScreen/ConnectionCell/Views/ConnectLabelAndNumberView.swift
@@ -0,0 +1,169 @@
+//
+//  ConnectLabelAndNumberView.swift
+//  yota
+//
+//  Created by Anton on 31.08.2020.
+//  Copyright © 2020 MST. All rights reserved.
+//
+
+import UIKit
+
+final class ConnectionLabelAndNumberView: UIView {
+
+    // MARK: - Property list
+
+    private let connectionLabel = UILabel()
+    private let numberView = UIView()
+    private let numberLabel = UILabel()
+
+    // MARK: - Initialization
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+
+        setup()
+    }
+
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Internal methods
+
+    func setText(_ connectionLabelText: String, numberLabelText: String) {
+        connectionLabel.text = connectionLabelText
+        numberLabel.text = numberLabelText
+
+        connectionLabel.addGradient(colors: [UIColor.Gradient.firstColor, UIColor.Gradient.secondColor])
+    }
+
+    // MARK: - Private methods
+
+    private func setupForIPhoneX() {
+        connectionLabel.font = .circeBold(size: 16)
+        numberLabel.font = .circeBold(size: 16)
+
+        NSLayoutConstraint.activate([
+            connectionLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 14),
+            connectionLabel.topAnchor.constraint(equalTo: topAnchor, constant: 14),
+            connectionLabel.heightAnchor.constraint(equalToConstant: 24),
+            connectionLabel.widthAnchor.constraint(equalToConstant: 50),
+
+            numberView.topAnchor.constraint(equalTo: topAnchor, constant: 10),
+            numberView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -14),
+            numberView.widthAnchor.constraint(equalToConstant: 146),
+            numberView.bottomAnchor.constraint(equalTo: bottomAnchor),
+
+            numberLabel.topAnchor.constraint(equalTo: numberView.topAnchor, constant: 4),
+            numberLabel.trailingAnchor.constraint(equalTo: numberView.trailingAnchor, constant: -10),
+            numberLabel.leadingAnchor.constraint(equalTo: numberView.leadingAnchor, constant: 10),
+            numberLabel.bottomAnchor.constraint(equalTo: numberView.bottomAnchor)
+        ])
+    }
+
+    private func setupForIPhone6Plus() {
+        connectionLabel.font = .circeBold(size: 16)
+        numberLabel.font = .circeBold(size: 16)
+
+        NSLayoutConstraint.activate([
+            connectionLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 16),
+            connectionLabel.topAnchor.constraint(equalTo: topAnchor, constant: 16),
+            connectionLabel.heightAnchor.constraint(equalToConstant: 24),
+            connectionLabel.widthAnchor.constraint(equalToConstant: 50),
+
+            numberView.topAnchor.constraint(equalTo: topAnchor, constant: 9),
+            numberView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -16),
+            numberView.widthAnchor.constraint(equalToConstant: 142),
+            numberView.bottomAnchor.constraint(equalTo: bottomAnchor),
+
+            numberLabel.leadingAnchor.constraint(equalTo: numberView.leadingAnchor, constant: 8),
+            numberLabel.topAnchor.constraint(equalTo: numberView.topAnchor, constant: 6),
+            numberLabel.trailingAnchor.constraint(equalTo: numberView.trailingAnchor, constant: -8),
+            numberLabel.bottomAnchor.constraint(equalTo: numberView.bottomAnchor, constant: -6)
+        ])
+    }
+
+    private func setupForIPhone6() {
+        connectionLabel.font = .circeBold(size: 15)
+        numberLabel.font = .circeBold(size: 16)
+
+        NSLayoutConstraint.activate([
+            connectionLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 16),
+            connectionLabel.topAnchor.constraint(equalTo: topAnchor, constant: 12),
+            connectionLabel.heightAnchor.constraint(equalToConstant: 22),
+            connectionLabel.widthAnchor.constraint(equalToConstant: 47),
+
+            numberView.topAnchor.constraint(equalTo: topAnchor, constant: 10),
+            numberView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -16),
+            numberView.widthAnchor.constraint(equalToConstant: 144),
+            numberView.bottomAnchor.constraint(equalTo: bottomAnchor),
+
+            numberLabel.leadingAnchor.constraint(equalTo: numberView.leadingAnchor, constant: 8),
+            numberLabel.topAnchor.constraint(equalTo: numberView.topAnchor, constant: 6),
+            numberLabel.trailingAnchor.constraint(equalTo: numberView.trailingAnchor, constant: -8),
+            numberLabel.bottomAnchor.constraint(equalTo: numberView.bottomAnchor, constant: -6)
+        ])
+    }
+
+    private func setupForIPhone5() {
+        connectionLabel.font = .circeBold(size: 15)
+        numberLabel.font = .circeBold(size: 16)
+
+        NSLayoutConstraint.activate([
+            connectionLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 14),
+            connectionLabel.topAnchor.constraint(equalTo: topAnchor, constant: 12),
+            connectionLabel.heightAnchor.constraint(equalToConstant: 22),
+            connectionLabel.widthAnchor.constraint(equalToConstant: 47),
+
+            numberView.topAnchor.constraint(equalTo: topAnchor, constant: 9),
+            numberView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -14),
+            numberView.widthAnchor.constraint(equalToConstant: 142),
+            numberView.bottomAnchor.constraint(equalTo: bottomAnchor),
+
+            numberLabel.leadingAnchor.constraint(equalTo: numberView.leadingAnchor, constant: 8),
+            numberLabel.topAnchor.constraint(equalTo: numberView.topAnchor, constant: 6),
+            numberLabel.trailingAnchor.constraint(equalTo: numberView.trailingAnchor, constant: -8),
+            numberLabel.bottomAnchor.constraint(equalTo: numberView.bottomAnchor, constant: -6)
+        ])
+    }
+
+    private func setupForIPad() {
+        connectionLabel.font = .circeBold(size: 24)
+        numberLabel.font = .circeBold(size: 18)
+
+        NSLayoutConstraint.activate([
+            connectionLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 24),
+            connectionLabel.topAnchor.constraint(equalTo: topAnchor, constant: 18),
+            connectionLabel.heightAnchor.constraint(equalToConstant: 35),
+            connectionLabel.widthAnchor.constraint(equalToConstant: 75),
+
+            numberView.topAnchor.constraint(equalTo: topAnchor, constant: 12),
+            numberView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -24),
+            numberView.widthAnchor.constraint(equalToConstant: 162),
+            numberView.bottomAnchor.constraint(equalTo: bottomAnchor),
+
+            numberLabel.leadingAnchor.constraint(equalTo: numberView.leadingAnchor, constant: 10),
+            numberLabel.topAnchor.constraint(equalTo: numberView.topAnchor, constant: 10),
+            numberLabel.trailingAnchor.constraint(equalTo: numberView.trailingAnchor, constant: -10),
+            numberLabel.bottomAnchor.constraint(equalTo: numberView.bottomAnchor, constant: -10)
+        ])
+    }
+
+    private func setup() {
+        [connectionLabel, numberView].forEach {
+            addSubview($0)
+            $0.translatesAutoresizingMaskIntoConstraints = false
+        }
+        numberView.addSubview(numberLabel)
+        numberLabel.translatesAutoresizingMaskIntoConstraints = false
+
+        switch UIDevice.screenType {
+        case .iphoneX: setupForIPhoneX()
+        case .iphone6plus: setupForIPhone6Plus()
+        case .iphone6: setupForIPhone6()
+        case .iphone5: setupForIPhone5()
+        case .ipad: setupForIPad()
+        }
+    }
+}
---
+++ yota/MainScreen/ConnectionCell/Views/PriceView.swift
@@ -0,0 +1,152 @@
+//
+//  PriceView.swift
+//  yota
+//
+//  Created by Anton on 31.08.2020.
+//  Copyright © 2020 MST. All rights reserved.
+//
+
+import UIKit
+
+final class PriceView: UIView {
+
+    // MARK: - Property list
+
+    private let priceDescriptionLabel = UILabel()
+    private let priceLabel = UILabel()
+    private let separatorView = UIView()
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+
+        setup()
+    }
+
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Private methods
+
+    private func setupForIPhoneX() {
+        priceDescriptionLabel.font = .circeRegular(size: 16)
+        priceLabel.font = .circeBold(size: 26)
+        NSLayoutConstraint.activate([
+            priceDescriptionLabel.topAnchor.constraint(equalTo: topAnchor, constant: 21),
+            priceDescriptionLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 14),
+            priceDescriptionLabel.heightAnchor.constraint(equalToConstant: 24),
+            priceDescriptionLabel.widthAnchor.constraint(equalToConstant: 24),
+
+            priceLabel.topAnchor.constraint(equalTo: topAnchor, constant: 8),
+            priceLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -14),
+            priceLabel.heightAnchor.constraint(equalToConstant: 64),
+            priceLabel.widthAnchor.constraint(equalToConstant: 38),
+            priceLabel.bottomAnchor.constraint(equalTo: separatorView.topAnchor, constant: -10),
+
+            separatorView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 14),
+            separatorView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -14),
+            separatorView.bottomAnchor.constraint(equalTo: bottomAnchor),
+            separatorView.heightAnchor.constraint(equalToConstant: 1)
+        ])
+    }
+
+    private func setupForIPhone6Plus() {
+        priceDescriptionLabel.font = .circeRegular(size: 16)
+        priceLabel.font = .circeBold(size: 26)
+        NSLayoutConstraint.activate([
+            priceDescriptionLabel.topAnchor.constraint(equalTo: topAnchor, constant: 26),
+            priceDescriptionLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 16),
+            priceDescriptionLabel.heightAnchor.constraint(equalToConstant: 24),
+            priceDescriptionLabel.widthAnchor.constraint(equalToConstant: 175),
+
+            priceLabel.topAnchor.constraint(equalTo: topAnchor, constant: 16),
+            priceLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -16),
+            priceLabel.widthAnchor.constraint(equalToConstant: 64),
+            priceLabel.bottomAnchor.constraint(equalTo: separatorView.topAnchor, constant: -13),
+
+            separatorView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 16),
+            separatorView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -16),
+            separatorView.bottomAnchor.constraint(equalTo: bottomAnchor),
+            separatorView.heightAnchor.constraint(equalToConstant: 1)
+        ])
+    }
+
+    private func setupForIPhone6() {
+        priceDescriptionLabel.font = .circeRegular(size: 15)
+        priceLabel.font = .circeBold(size: 24)
+
+        NSLayoutConstraint.activate([
+            priceDescriptionLabel.topAnchor.constraint(equalTo: topAnchor, constant: 16),
+            priceDescriptionLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 16),
+            priceDescriptionLabel.heightAnchor.constraint(equalToConstant: 22),
+            priceDescriptionLabel.widthAnchor.constraint(equalToConstant: 164),
+
+            priceLabel.topAnchor.constraint(equalTo: topAnchor, constant: 7),
+            priceLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -16),
+            priceLabel.widthAnchor.constraint(equalToConstant: 59),
+            priceLabel.bottomAnchor.constraint(equalTo: separatorView.topAnchor, constant: -5),
+
+            separatorView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 16),
+            separatorView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -16),
+            separatorView.heightAnchor.constraint(equalToConstant: 1),
+            separatorView.bottomAnchor.constraint(equalTo: bottomAnchor)
+        ])
+    }
+
+    private func setupForIPhone5() {
+        priceDescriptionLabel.font = .circeRegular(size: 15)
+        priceLabel.font = .circeBold(size: 20)
+        NSLayoutConstraint.activate([
+            priceDescriptionLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 14),
+            priceDescriptionLabel.topAnchor.constraint(equalTo: topAnchor, constant: 15),
+            priceDescriptionLabel.heightAnchor.constraint(equalToConstant: 22),
+            priceDescriptionLabel.widthAnchor.constraint(equalToConstant: 164),
+
+            priceLabel.topAnchor.constraint(equalTo: topAnchor, constant: 10),
+            priceLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -14),
+            priceLabel.heightAnchor.constraint(equalToConstant: 29),
+            priceLabel.bottomAnchor.constraint(equalTo: separatorView.topAnchor, constant: -5),
+
+            separatorView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 14),
+            separatorView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -14),
+            separatorView.bottomAnchor.constraint(equalTo: bottomAnchor),
+            separatorView.heightAnchor.constraint(equalToConstant: 1)
+        ])
+    }
+
+    private func setupForIPad() {
+        priceDescriptionLabel.font = .circeRegular(size: 20)
+        priceLabel.font = .circeBold(size: 38)
+        NSLayoutConstraint.activate([
+            priceDescriptionLabel.topAnchor.constraint(equalTo: topAnchor, constant: 25),
+            priceDescriptionLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 24),
+            priceDescriptionLabel.heightAnchor.constraint(equalToConstant: 29),
+            priceDescriptionLabel.widthAnchor.constraint(equalToConstant: 219),
+
+            priceLabel.topAnchor.constraint(equalTo: topAnchor, constant: 6),
+            priceLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -24),
+            priceLabel.bottomAnchor.constraint(equalTo: separatorView.topAnchor, constant: -3),
+
+            separatorView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 24),
+            separatorView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -24),
+            separatorView.bottomAnchor.constraint(equalTo: bottomAnchor),
+            separatorView.heightAnchor.constraint(equalToConstant: 1)
+        ])
+    }
+
+    private func setup() {
+        [priceDescriptionLabel, priceLabel, separatorView].forEach {
+            $0.translatesAutoresizingMaskIntoConstraints = false
+            addSubview($0)
+        }
+
+        switch UIDevice.screenType {
+        case .iphoneX: setupForIPhoneX()
+        case .iphone6plus: setupForIPhone6Plus()
+        case .iphone6: setupForIPhone6()
+        case .iphone5: setupForIPhone5()
+        case .ipad: setupForIPad()
+        }
+    }
+}
