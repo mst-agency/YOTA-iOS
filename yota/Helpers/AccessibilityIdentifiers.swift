a6b5a4853230ae5c09731f162b23eaa7b6eac9c1
Anton Belov <anb@mstagency.ru>
2020-09-07 10:40:43 +0300

    Added accessibility idenfitifiers
    # Conflicts:
    #	yota.xcodeproj/project.pbxproj
    #	yota/MainScreen/ConnectionCell/Views/ConnectLabelAndNumberView.swift
    #	yota/MainScreen/ConnectionCell/Views/PriceView.swift
    #	yota/MainScreen/ConnectionCell/Views/UnlimitedAppsView.swift

---
+++ yota/Helpers/AccessibilityIdentifiers.swift
@@ -0,0 +1,24 @@
+//
+//  AccessibilityIdentifiers.swift
+//  yota
+//
+//  Created by Anton on 07.09.2020.
+//  Copyright © 2020 MST. All rights reserved.
+//
+
+import Foundation
+
+struct AccessibilityIdentifiers {
+
+    struct ConnectionModule {
+
+        static let connectionLabel = "connectionLabel"
+        static let numberView = "numberView"
+        static let numberLabel = "numberLabel"
+        static let unlimitedAppsLabel = "unlimitedAppsLabel"
+        static let unlimitedAppsImages = "unlimitedAppsImages"
+        static let priceDescriptionLabel = "priceDescriptionLabel"
+        static let priceLabel = "priceLabel"
+        static let separatorView = "separatorView"
+    }
+}
